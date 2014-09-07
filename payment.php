41cf9b077b73e7a4e1cc8f3205c1e4022d07dcc7
Vasu Jain <vasu.jain@live.in>
2014-09-07 13:58:15 -0700

    formatting fix

--- payment.php
+++ payment.php
@@ -21,8 +21,10 @@ include 'header.php';
 
         <div class="row centered" style="padding-bottom:20px;">
             <div id="hsocial" class="col-lg-8 col-lg-offset-2">
-                <<div class="col-md-4">
-                    <a href="http://windowsvj.com/@ps/wth/modules/payment/index.php?amount=120&currency=USD"><button type="button" class="btn btn-theme btn-lg">MasterCard</button></a>
+                <div class="col-md-4">
+                    <a href="http://windowsvj.com/@ps/wth/modules/payment/index.php?amount=120&currency=USD">
+                        <button type="button" class="btn btn-theme btn-lg">MasterCard</button>
+                    </a>
                 </div>
                 <div class="col-md-4">
                     <a href="paymentconfirm.php?mode=bitpay"><button type="button" class="btn btn-theme btn-lg">BitPay</button></a>
@@ -31,11 +33,6 @@ include 'header.php';
                     <a href="paymentconfirm.php?mode=others"> <button type="button" class="btn btn-theme btn-lg">Others</button></a>
                 </div>
             </div><!-- /social -->
-        </div>
-
-        <!-- OR? -->
-        <div class="row centered">
-            <h2> OR </h2>
         </div>
 
         <div class="row centered">
